c3f1031f15ec3182d431d7194786b35f55015b4e
datodato788 <gogiawenom8@gmail.com>
2025-09-19 18:48:19 +0400

    init: create base web structure

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var navIcon = document.getElementById('nav-icon3');
+  var aside = document.querySelector('.div-aside');
+  
+  navIcon.addEventListener('click', function() {
+    navIcon.classList.toggle('open');
+    aside.classList.toggle('active'); 
+  });
+});
+
+const collapsibleItems = document.querySelectorAll(".collapsible");
+
+collapsibleItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("active");
+  });
+});
